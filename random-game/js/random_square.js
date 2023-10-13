421b7f897d85e43e7113629fd0a3620dc7f2d39e
Denis Dankin <ulman33312@gmail.com>
2023-10-14 00:22:37 +0300

    feat: add change color square (Sat, Oct 14, 2023 00:22:45 AM)

--- random-game/js/random_square.js
+++ random-game/js/random_square.js
@@ -2,8 +2,7 @@ export class Random_square {
   constructor(squareItem) {
     this.randomSquareItem = document.createElement('div');
     this.randomSquareItem.classList.add('game__square-displacement');
-    this.value = Math.random() > 0.5 ? 2 : 4;
-    this.randomSquareItem.textContent = this.value.toString();
+    this.setValue(Math.random() > 0.5 ? 2 : 4);
     squareItem.append(this.randomSquareItem);
   }
 
@@ -12,5 +11,13 @@ export class Random_square {
     this.y = y;
     this.randomSquareItem.style.setProperty('--x', x);
     this.randomSquareItem.style.setProperty('--y', y);
+  }
+
+  setValue(randomSquare) {
+    this.value = randomSquare;
+    this.randomSquareItem.textContent = this.value.toString();
+    const bgColor = 100 - Math.log2(randomSquare) * 8;
+    this.randomSquareItem.style.setProperty('--bg-color', `${bgColor}%`);
+    this.randomSquareItem.style.setProperty('--color', `${bgColor < 50 ? 90 : 10}%`);
   }
 }
--- random-game/js/scripts.js
+++ random-game/js/scripts.js
@@ -4,4 +4,4 @@ import {Random_square} from "./random_square.js";
 const gameField = document.getElementById('game');
 
 const square = new Squares(gameField);
-square.getRandomSquare().linkRandomSquare(new Random_square(gameField))
+square.getRandomSquare().linkRandomSquare(new Random_square(gameField));
